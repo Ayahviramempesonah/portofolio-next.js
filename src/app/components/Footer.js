cd3e2357638d159803b883cfbdd0e1805095940c
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2025-01-01 19:39:21 +0700

    updat

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="w-full bg-gray-800 text-white py-4 md:py-6">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-          <p className="text-sm md:text-base mb-4 md:mb-0">&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+          <p className="text-sm md:text-base mb-4 md:mb-0">&copy; {new Date().getFullYear()} coderTamvan. All rights reserved.</p>
           <div className="flex gap-4 mt-2 md:mt-0">
             <a
               href="https://twitter.com"
