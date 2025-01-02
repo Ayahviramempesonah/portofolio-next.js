3b7e19299937816d093a0caae093b87ce22ca909
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2025-01-03 02:54:24 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default async function HomePage() {
                   <img
                     src={product.image}
                     alt={product.title}
-                    style={{ width: "100%", height: "100px", objectFit: "contain" }}
+                    style={{ width: "100%", height: "75px", objectFit: "contain" }}
                   />
                   <h3 style={{ marginTop: "8px" ,overflow:"hidden"}}>{product.title}</h3>
                   <p> ${ product.price}</p>
